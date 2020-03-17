725725186b2d54b8ca661f3e3c5893cd909a07db
yuvarathna <62106588+yuvarathna@users.noreply.github.com>
2020-03-17 21:33:18 +0530

    Update settings.py

--- brandstore/settings.py
+++ brandstore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '30b-xmcq#$b^3bh1n^_r1!y1-xop=awzb%cthh&w_j4c3ky-u('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['yuvabrandstore.herokuapp.com']
 

bf4a4f4d6f8d90a09cc3986a329fa85b786b46a0
yuvarathna <yuvarathna9999@gmial.com>
2020-04-17 13:16:36 +0530

    added cart and items

--- website/models.py
+++ website/models.py
@@ -42,6 +42,8 @@ class Item(models.Model):
             'slug':self.slug
         })
 
+
+
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -53,6 +55,24 @@ class OrderItem(models.Model):
         return f"{self.quantity} of {self.item.title}"
 
 
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+    
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+
+    
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
+
+
+
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -60,3 +80,16 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+
+
+    def __str__(self):
+        return self.user.username
+
+
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
